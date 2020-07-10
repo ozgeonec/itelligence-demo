ce01363331e53dd24aa5672bc76a2754bc308eea
ozgeonec <ozgenuronec@gmail.com>
2020-07-10 13:41:52 +0300

    demo

---
+++ src/main/java/com/example/itelligence/ItelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.itelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itelligence/ItelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
